851ed7beef78e33557d583023e1de7f1995b3b53
Daniel Berger <djberg96@gmail.com>
2014-11-08 08:08:02 -0700

    Conditionally defined String#wincode to avoid warnings.

--- lib/win32/file/windows/functions.rb
+++ lib/win32/file/windows/functions.rb
@@ -40,9 +40,11 @@ module Windows
 end
 
 class String
-  # Convenience method for converting strings to UTF-16LE for wide character
-  # functions that require it.
-  def wincode
-    (self.tr(File::SEPARATOR, File::ALT_SEPARATOR) + 0.chr).encode('UTF-16LE')
+  unless instance_methods.include?(:wincode)
+    # Convenience method for converting strings to UTF-16LE for wide character
+    # functions that require it.
+    def wincode
+      (self.tr(File::SEPARATOR, File::ALT_SEPARATOR) + 0.chr).encode('UTF-16LE')
+    end
   end
 end
