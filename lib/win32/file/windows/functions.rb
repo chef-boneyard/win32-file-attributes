f5f87e601a0ac726c38ff3f6b83ddc29724ab301
Daniel Berger <djberg96@gmail.com>
2014-11-08 07:17:59 -0700

    Make FFI functions private.

--- lib/win32/file/windows/functions.rb
+++ lib/win32/file/windows/functions.rb
@@ -4,16 +4,25 @@ module Windows
   module File
     module Functions
       extend FFI::Library
+
+      private
+
+      # Wrapper method for attach_function + private
+      def self.attach_pfunc(*args)
+        attach_function(*args)
+        private args[0]
+      end
+
       typedef :ulong, :dword
       typedef :uintptr_t, :handle
 
       ffi_lib :kernel32
 
-      attach_function :CloseHandle, [:handle], :bool
-      attach_function :CreateFileW, [:buffer_in, :dword, :dword, :pointer, :dword, :dword, :handle], :handle
-      attach_function :DeviceIoControl, [:handle, :dword, :pointer, :dword, :pointer, :dword, :pointer, :pointer], :bool
-      attach_function :GetFileAttributesW, [:buffer_in], :dword
-      attach_function :SetFileAttributesW, [:buffer_in, :dword], :bool
+      attach_pfunc :CloseHandle, [:handle], :bool
+      attach_pfunc :CreateFileW, [:buffer_in, :dword, :dword, :pointer, :dword, :dword, :handle], :handle
+      attach_pfunc :DeviceIoControl, [:handle, :dword, :pointer, :dword, :pointer, :dword, :pointer, :pointer], :bool
+      attach_pfunc :GetFileAttributesW, [:buffer_in], :dword
+      attach_pfunc :SetFileAttributesW, [:buffer_in, :dword], :bool
 
       def CTL_CODE(device, function, method, access)
          ((device) << 16) | ((access) << 14) | ((function) << 2) | (method)
